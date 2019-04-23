a0320e869f14db4f4a36f40e28d7ecdca943c3ee
Billy Joseph <ingbillyjoseph@gmail.com>
2019-04-23 15:35:49 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,5 +1,28 @@
-# code here!
 class School
-school = School.new 
-school.roster 
-end 
+
+  attr_accessor :roster
+
+  def initialize(name)
+    @name = name
+    @roster = Hash.new { |h,k| h[k] = []}
+  end
+
+  def add_student(name, year)
+    @roster[year] << name
+  end
+
+  def grade(year)
+    @roster[year]
+  end
+
+  def sort
+    Hash[@roster.collect{ |a| [a.first, sort_students(a.first)] }]
+  end
+
+  private # cannot be called with an explicit receiver. only implicit receiver
+
+  def sort_students(year)
+   grade(year).sort
+  end
+
+end
